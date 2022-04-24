452418f32e6bdc13ed753fc5bdec2be7972baf23
David Toro Arango <92808109+DavidToro1000@users.noreply.github.com>
2022-04-24 12:47:04 -0500

    Create reptil.py

---
+++ zooAnimales/reptil.py
@@ -0,0 +1,34 @@
+from animal import Animal
+class Reptil(Animal):
+    _listado=[]
+    iguanas=0
+    serpientes=0
+    def __init__(self,nombre,edad,habitat,genero, colorEscamas, largoCola):
+        super().__init__(self,nombre,edad,habitat,genero)
+        self._colorEscamas=colorEscamas
+        self._largoCola=largoCola
+        Reptil._listado.append(self)
+    def setColorEscamas(self,c):
+        self._colorEscamas=c
+    def getcolorEscamas(self):
+        return self._colorEscamas
+    def setlargoCola(self,l):
+        self._largoCola=l
+    def getlargoCola(self):
+        return self._largoCola
+    @classmethod
+    def getListado(cls):
+        return(cls._listado)
+    @classmethod
+    def cantidadReptiles(cls):
+        return(len(cls._listado))
+    @classmethod
+    def crearIguana(cls, nombre, edad, genero):
+        i=Reptil(nombre,edad,"humedal",genero,"verde", 3)
+        cls.iguanas+=1
+        return i
+    @classmethod
+    def crearSerpiente(cls, nombre, edad, genero):
+        s = Reptil(nombre, edad, "jungla", genero, "blanco",1)
+        cls.serpientes += 1
+        return s

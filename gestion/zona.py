14626e1083c7c0bf210d085e4b9901db2adb5b3e
David Toro Arango <92808109+DavidToro1000@users.noreply.github.com>
2022-04-24 13:22:47 -0500

    Update zona.py

--- gestion/zona.py
+++ gestion/zona.py
@@ -1,8 +1,8 @@
 class Zona():
-    def __init__(self, nombre, zoo=None, animales=[]):
+    def __init__(self, nombre, zoo=None):
         self._nombre=nombre
         self._zoo=zoo
-        self._animales=animales
+        self._animales=[]
     def agregarAnimales (self, animal):
         self._animales.append(animal)
     def cantidadAnimales(self):

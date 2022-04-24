96851136712aa34dc25a912d1d93ca9286a759fc
David Toro Arango <92808109+DavidToro1000@users.noreply.github.com>
2022-04-24 13:05:24 -0500

    Update mamifero.py

--- zooAnimales/mamifero.py
+++ zooAnimales/mamifero.py
@@ -4,7 +4,7 @@ class Mamifero(Animal):
     caballos=0
     leones=0
     def __init__(self,nombre,edad,habitat,genero, pelaje, patas):
-        super().__init__(self,nombre,edad,habitat,genero)
+        super().__init__(nombre,edad,habitat,genero)
         self._pelaje=pelaje
         self._patas=patas
         Mamifero._listado.append(self)
